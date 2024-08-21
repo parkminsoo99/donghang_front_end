56cb3b832a814160550b2a70113e137d8c777be5
parkminsoo99 <zzangorc99@naver.com>
2024-08-21 17:00:08 +0900

    [FIX] applied style SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import StyledComponentsRegistry from './lib/registry';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
+        <StyledComponentsRegistry>
+          <AntdRegistry>{children}</AntdRegistry>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
